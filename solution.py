565213aba658e29a17d4fdf9190ad8f5b367ea2f
Margo-Sh <67891965+Margo-Sh@users.noreply.github.com>
2024-05-23 20:43:06 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 334982039 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    result = sps.anderson_ksamp([x, y])
+
+    # Check if null hypothesis is rejected at alpha=0.02
+    return result.significance_level < 0.02
